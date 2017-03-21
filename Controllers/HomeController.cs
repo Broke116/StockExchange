f03ca6f82ebb939c2f6811a53d07f34572111f71
Broke116 <ekiny018@gmail.com>
2017-03-21 10:26:18 +0300

    see stock detail

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using StockExchange.Service;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using StockExchange.Models;
 
 namespace StockExchange.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var service = new DataService();
-            var list = service.GetData();
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync("http://localhost:5000/api/stockapi");
+                var model = JsonConvert.DeserializeObject<List<StockModel>>(
+                response.Content.ReadAsStringAsync().Result);
+                return View(model);
+            }            
+        }
 
-            return View(list);
+        [Route("show/{title}")]
+        public async Task<ActionResult> Index(string title)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("http://localhost:5000/api/stockapi/"+title);
+                var data = JsonConvert.DeserializeObject<StockModel>(
+                response.Content.ReadAsStringAsync().Result);
+                var model=new List<StockModel>();   
+                model.Add(data);             
+                return View(model);
+            }            
         }
 
         public IActionResult About()
